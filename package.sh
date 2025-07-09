9ae99e813eab593c3e65675d7a69926d0ec6d575
mrhihi <1590656+mrhihi@users.noreply.github.com>
2025-07-09 12:47:22 +0800

    加入 gitignore 以及打包的腳本

---
+++ package.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+ZIP_NAME="hitextclip.zip"
+
+# 移除舊的 zip 檔
+rm -f $ZIP_NAME
+
+# 打包，排除 .git、README.md、腳本本身及 macOS 隱藏檔
+zip -r $ZIP_NAME . \
+    -x "*.git*" \
+    -x "README.md" \
+    -x "package.sh" \
+    -x "*.DS_Store"
+
+echo "打包完成：$ZIP_NAME"
